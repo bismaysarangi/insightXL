e887f8868589d1d84346f57806e5efcb961db1b1
Bismay <bismaysarangi@gmail.com>
2025-05-21 20:20:04 +0530

    feat: update @radix-ui/react-slot to version 1.2.3 and add Badge component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
 
 function App() {
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
       <Button>Click me</Button>
+      <Badge className="bg-blue-500 text-white">Hello</Badge>
     </div>
   );
 }

2711d13071ba1bfc88fdcdd877161d653b645f7e
Bismay <bismaysarangi@gmail.com>
2025-05-23 19:25:35 +0530

    feat: restructure App component and add Navbar and Footer components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,9 @@
-import { Button } from "@/components/ui/button";
-import { Badge } from "@/components/ui/badge";
+import Home from "./pages/home";
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Click me</Button>
-      <Badge className="bg-blue-500 text-white">Hello</Badge>
+    <div>
+      <Home />
     </div>
   );
 }

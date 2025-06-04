b55328185ddf50c547e13a50bf716e908df0e1f0
Bismay <bismaysarangi@gmail.com>
2025-06-04 21:52:30 +0530

    feat: add authentication pages with form handling and UI enhancements

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import AuthPages from "./AuthPages";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <AuthPages />
       <Footer />
     </div>
   );

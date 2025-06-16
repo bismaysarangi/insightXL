a83b81b591ffadfbacff5dac401c0c08b9f67e4d
Bismay <bismaysarangi@gmail.com>
2025-06-16 22:45:00 +0530

    Add user model, authentication routes, and configure server with body-parser and CORS

---
+++ server/Routes/AuthRouter.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+router.post('/login', (req, res) => {
+    res.send('Login successful');
+});
+
+router.post('/signup', (req, res) => {
+    res.send('Signup successful');
+});
+
+module.exports = router;
